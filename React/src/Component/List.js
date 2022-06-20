63d4ae08790d4f24b41319778d18ffd333a4a96f
adil waheed <adilwaheed2222.com@gmail.com>
2022-06-20 20:52:09 +0500

    React Assignment 3

---
+++ React/src/Component/List.js
@@ -0,0 +1,62 @@
+import img from '../Assets/pro1.png'
+import img1 from '../Assets/pro2.png'
+import img2 from '../Assets/pro3.png'
+
+export const List = [
+    {
+      id: 1,
+      images:`${img}`,
+      name: 'Stop Watch',
+      git: 'https://github.com/adilwaheed786/JavaScript_StopWatch',
+      live: 'https://adilwaheed786.github.io/JavaScript_StopWatch/'
+    },
+    {
+      id: 2,
+      images:`${img1}`,
+      name: 'Admin Panel',
+      git: 'https://github.com/adilwaheed786/Sda_project',
+      live: 'https://github.com/adilwaheed786/Sda_project'
+    },
+    {
+      id: 3,
+      images:`${img2}`,
+      name: 'Coming Soon',
+      git: '',
+      live: ''
+    },
+    {
+      id: 4,
+      images:`${img2}`,
+      name: 'Coming Soon',
+      git: '',
+      live: ''
+    },
+    {
+      id: 5,
+      images:`${img2}`,
+      name: 'Coming Soon',
+      git: '',
+      live: ''
+    },
+    {
+      id: 6,
+      images:`${img2}`,
+      name: 'Coming Soon',
+      git: '',
+      live: ''
+    },
+    {
+      id: 7,
+      images:`${img2}`,
+      name: 'Coming Soon',
+      git: '',
+      live: ''
+    },
+    {
+      id: 8,
+      images:`${img2}`,
+      name: 'Coming Soon',
+      git: '',
+      live: ''
+    }
+  ];
